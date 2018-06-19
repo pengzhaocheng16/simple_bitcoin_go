fac8761988cba07d5eb2360ef1358c14c5147666
pengzhaocheng16 <pengzhch4213@sina.com>
2018-06-20 00:37:43 +0800

    update dotray nat mapping check loopback

--- dotray/node.go
+++ dotray/node.go
@@ -71,12 +71,15 @@ func StartNode(laddr, saddr string, send, recv chan interface{}) error {
 	quit := make(chan struct{})
 	Nat :=nat.Any()
 	var realaddr *net.TCPAddr
-	go func() {
-		nat.Map(Nat, quit, "tcp", laddr1.Port, laddr1.Port, "ethereum p2p")
-	}()
-	// TODO: react to external IP changes over time.
-	if ext, err := Nat.ExternalIP(); err == nil {
-		realaddr = &net.TCPAddr{IP: ext, Port: realaddr.Port}
+
+	if !laddr1.IP.IsLoopback() && Nat != nil {
+		go func() {
+			nat.Map(Nat, quit, "tcp", laddr1.Port, laddr1.Port, "ethereum p2p")
+		}()
+		// TODO: react to external IP changes over time.
+		if ext, err := Nat.ExternalIP(); err == nil {
+			realaddr = &net.TCPAddr{IP: ext, Port: realaddr.Port}
+		}
 	}
 
 	// wait for downstream nodes to connect
